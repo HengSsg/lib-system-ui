a2c9260a53ecdc4f822cc8be2f0a13ab14814b1b
HyungSeok Park <dnflekf2748@gmail.com>
2023-06-17 16:27:02 +0900

    Update rent.js

--- admin/rent.js
+++ admin/rent.js
@@ -1,6 +1,6 @@
 // script.js 파일의 내용
 let user;
-document.getElementById("searchBtn").addEventListener("click", fetchJquery);
+document.getElementById("searchBtn").addEventListener("click", fetchAjax);
 
 document.getElementById("ReturnBtn").addEventListener("click", function () {
   let checkedData = [];
@@ -139,4 +139,84 @@ function fetchJquery() {
       });
     });
   });
+}
+
+function fetchAjax() {
+  $.ajax({
+    url: "rent.json",
+    method: "get",
+    dataType: "json",
+    success: function (data) {
+      console.log(data.userData);
+    },
+    error: function (error) {
+      console.log("AJAX 요청 실패:", error);
+    },
+  });
+  $.ajax({
+    url: "rent.json",
+    method: "get",
+    dataType: "json",
+  });
+  // $(document).ready(function () {
+  //   $("#searchBtn").click(function () {
+  //     $.getJSON("rent.json", function (data) {
+  //       let id = document.getElementById("searchID").value;
+
+  //       console.log(user);
+  //       for (let i = 0; i < data.userData.length; i++) {
+  //         let userInfo = data.userData[i];
+  //         if (userInfo.id == parseInt(id)) {
+  //           user = userInfo;
+  //         }
+  //       }
+
+  //       if (user != undefined) {
+  //         // id값이 해당하는 것이 있으면 실행
+  //         document.getElementById("name").textContent = user.name;
+  //         document.getElementById("ID").textContent = user.id;
+  //         document.getElementById("rentNum").textContent = user.rentNum;
+  //         document.getElementById("status").textContent = user.status;
+  //         document.querySelector(".searchBook").value = "";
+
+  //         var bookContainer = document.getElementById("bookData");
+
+  //         bookContainer.innerHTML = ""; // 초기화
+  //         for (let i = 0; i < user.bookData.length; i++) {
+  //           // innerHtml로 바꾸기
+  //           let name = user.bookData[i].name;
+  //           let author = user.bookData[i].author;
+  //           let publisher = user.bookData[i].publisher;
+  //           let rentDate = user.bookData[i].rentDate;
+  //           let returnDate = user.bookData[i].returnDate;
+
+  //           bookContainer.innerHTML += `
+  //           <div>
+  //             <div class="RbookContents">
+  //               <div>
+  //                 <input type="checkbox" name="color" value="${name}" class="check"/>
+  //               </div>
+  //               <div>
+  //                 <img src="../image/img0${i + 1}.jpg" class="Imageclass" />
+  //               </div>
+  //               <div class="RbookInfo">
+  //                 <div>
+  //                   <p>도서명: ${name}</p>
+  //                   <p>저자명: ${author}</p>
+  //                   <p>출판사: ${publisher}</p>
+  //                 </div>
+  //               </div>
+  //               <div class="RbookInfo2">
+  //                 <div>
+  //                   <p>대출 일자: ${rentDate}</p>
+  //                   <p>반납예정일: ${returnDate}</p>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </div>`;
+  //         }
+  //       }
+  //     });
+  //   });
+  // });
 }
